95e8cdb01c3612456d906c80dcde8f93d82c94c5
jyo545 <108000083+jyo545@users.noreply.github.com>
2022-06-24 13:15:30 +0530

    Update business_logic.py

--- business_logic.py
+++ business_logic.py
@@ -14,16 +14,14 @@ def subtraction(n1,n2):
     return result
 
 def division(n1,n2):
-    if n1 and n2:
-        if type(n1) == int and type(n2) == int:
-            if n2==0:
-                return "Second Number Cannot be Zero"
-            result = n1 / n2
-            return round(result ,2)
-        else:
-            return "Invalid Input"
+    if type(n1) == int and type(n2) == int:
+        if n2==0:
+            return "Second Number Cannot be Zero"
+        result = n1 / n2
+        return round(result ,2)
     else:
-        return "Inputs Required..cannot be blank...!"
+        return "Invalid Input"
+   
 def multiplication(n1,n2):
     if n1 and n2:
         if type(n1) == int and type(n2) == int:
